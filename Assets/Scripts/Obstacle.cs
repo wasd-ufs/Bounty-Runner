791c28338f0ab8c6002fb3bfa0ddcfff791c2769
KazumDevv <joaocarlosprocarvalho@gmail.com>
2024-04-17 20:25:55 -0300

    sla

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -6,11 +6,7 @@ public class Obstacle : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Obstacle")
-        {
-            Destroy(collision.gameObject);
-
-        }
+    
         if(transform.position.x < 0){
             Destroy(gameObject);
         }      
